379293fb54a1d98070b42a2d340c08d0acc2abd7
Jester Abraham <mail.musthak@gmail.com>
2025-07-14 10:19:48 +0530

    cahnged

--- lib/core/constants/chat_api_constants.dart
+++ lib/core/constants/chat_api_constants.dart
@@ -1,9 +1,9 @@
 class ChatApiConstants {
   // Base URLs
-  static const String chatBaseUrlPro = 'https://chat.prod.liveraapp.com/chat/';
+  static const String chatBaseUrlPro = 'https://chat.api.liveraapp.com/chat/';
   static const String chatBaseUrlDev = 'http://192.168.3.6:3555/chat/';
-  static const String chatBaseUrl = 'http://192.168.3.6:3555/chat/';
-  static const String chatSocketUrl = 'http://192.168.3.6:3555/';
+  static const String chatBaseUrl = 'https://chat.api.liveraapp.com/chat/';
+  static const String chatSocketUrl = 'https://chat.api.liveraapp.com/';
 
   // Chat Module Endpoints
 
